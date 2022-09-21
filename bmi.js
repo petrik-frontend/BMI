7b8651560d1b1b2364652db202366bf878b8c5a7
Elcsa <me0915@live.com>
2022-09-21 08:14:32 +0200

    bmi

---
+++ bmi.js
@@ -0,0 +1,21 @@
+function katt() {
+    document.getElementById('gomb').textContent = 'kalkulator gomb';
+    console.log('katt');
+}
+
+function szamol() {
+    let magasag = parseFloat(document.getElementById('magasag').value);
+    let suly = parseFloat(document.getElementById('suly').value);
+    dokument.getElementById('ereddmeny').value = suly / (magasag / 100) ** 2;
+
+}
+
+function init() {
+    document.getElementById('gomb').addEventListener('click', katt);
+    document.getElementById('magasag').addEventListener('change', szamol);
+    document.getElementById('suly').addEventListener('input', szamol);
+
+    console.log('init');
+}
+document.addEventListener('DOMContentLoaded', init);
+console.log('js fej vege')
